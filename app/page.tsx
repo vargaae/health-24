bf3166fbc14672dd43483782f56118e4c22a5da9
vargaae <vargaae@hotmail.com>
2024-09-03 10:07:15 +0200

    Setup Homepage + Add assets

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,53 @@
-import { Button } from "@/components/ui/button";
+import PatientForm from "@/components/forms/PatientForm";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-white text-3xl underline">Home</h1>
-      <Button variant="outline">Click me!</Button>
+    <div className="flex h-screen max-h-screen">
+      {/* TODO: OTP Verification */}
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+          <div className="flex">
+            <Image
+              src="/assets/icons/logo-full.svg"
+              height={1000}
+              width={1000}
+              alt="patient"
+              className="mb-12 h-10 w-fit"
+            />
+            <Link
+              href="/"
+              className="ml-2 mt-1 flex justify-between text-lg font-extrabold bg-gradient-to-r from-blue-400 via-green-500 to-white inline-block text-transparent bg-clip-text"
+            >
+              Health-24
+            </Link>
+            <Image
+              src="/assets/icons/logo-icon.svg"
+              height={1000}
+              width={1000}
+              alt="patient"
+              className="mb-12 h-10 w-fit ml-2 "
+            />
+          </div>
+          <PatientForm />
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">
+              © 2024 Health-24 by Andras Varga
+            </p>
+            <Link href="/?admin=true" className="text-green-500">
+              Admin
+            </Link>
+          </div>
+        </div>
+      </section>
+      <Image
+        src="/assets/images/onboarding-img.png"
+        height={1000}
+        width={1000}
+        alt="patient"
+        className="side-img max-w-[50%]"
+      />
     </div>
   );
 }
